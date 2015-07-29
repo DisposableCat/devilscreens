69dea21cc990be58b52e0835841cf161cd63e161
VerstandInvictus <peter.k.cawley@gmail.com>
2015-07-29 01:43:18 -0700

    this now generates icons from templates. To be integrated.

--- iconsassemble.py
+++ iconsassemble.py
@@ -1,9 +1,25 @@
 from PIL import Image
 
-def iconAssembler(button,  template, background):
-    buttonFilename = "\\themes\\" + template + "\\" + button + ".png"
-    back = Image.open("\\themes\\" + background)
-    icon = Image.open(buttonFilename)
-    final = Image.composite(back, icon)
-    final.show()
+button = "pause"
+template = "clearCircles"
+background = "back"
+color = "#000000"
 
+def iconAssembler(button, template, color, background):
+    back = Image.open("themes\\backgrounds\\" + background + ".png")
+    back = back.convert('RGBA')
+    icon = Image.open("themes\\" + template + "\\" + button + ".png")
+    icon = icon.convert('RGBA')
+    replaceStroke(icon, color)
+    mask = Image.open("themes\\" + template + "\\" + button + "Mask.png")
+    mask = mask.convert('L')
+#    mask.show()
+    back.putalpha(mask)
+#    back.show()
+    final = Image.alpha_composite(back, icon)
+    final.save("themes\\backgrounds\\test.png")
+
+def replaceStroke(icon, color):
+    for y in xrange(icon.size[0])
+
+iconAssembler(button, template, color, background)
